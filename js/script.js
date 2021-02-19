866c359e8dc77b06e5d3b023dd8958fa315fb476
Michele Cofano <brutalmachinelol@gmail.com>
2021-02-19 15:16:09 +0100

    navbar-top

--- js/script.js
+++ js/script.js
@@ -2,6 +2,43 @@ new Vue({
   el: '#app',
   data: {
 
+    // array nel quale inseriamo il nome dell'oggetto della "navbar centrale", che si può riempire e
+    // modificare anche dinamicamente
+
+    CenterNavbar: [
+      {
+        obj: 'Home',
+        choices: '',
+      },
+      {
+        obj: 'Pages',
+        choices: '',
+      },
+      {
+        obj: 'Courses',
+        choices: '',
+      },
+      {
+        obj: 'Features',
+        choices: '',
+      },
+      {
+        obj: 'Blog',
+        choices: '',
+      },
+      {
+        obj: 'Shop',
+        choices: '',
+      },
+
+    ],
+
+    selected: 'English',
+    options: [
+      { text: 'English', value: 'English' },
+      { text: 'Deutsche', value: 'Deutsche' },
+
+    ]
   },
 
   methods: {
