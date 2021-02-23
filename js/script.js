92ffed5c4159285eaee641fc26a51396bf1ca438
Michele Cofano <brutalmachinelol@gmail.com>
2021-02-23 10:44:14 +0100

    Added footer

--- js/script.js
+++ js/script.js
@@ -200,4 +200,80 @@ new Vue({
 
   },
 });
+
+// **************************************************************
+var footer= new Vue({
+  el:"#footer",
+  data: {
+    contact: {
+      address: "382 NE 191st St # 87394 Miami, FL 33179 - 33899",
+      telephone: "+1(305)547-99(9am - 5pm EST, Monday - Friday)",
+      email: "support@maxcoach.com"
+    },
+    firstcolumn: [
+      {
+        text: "Start here",
+        link:"#"
+      },
+      {
+        text: "Blog",
+        link:"#"
+      },
+      {
+        text: "About us",
+        link:"#"
+      }
+    ],
+    secondcolumn:[
+      {
+        text: "Success story",
+        link: "#"
+      },
+      {
+        text: "Courses",
+        link: "#"
+      },
+      {
+        text: "Contact us",
+        link: "#"
+      }
+    ],
+    thirdcolumn:[
+      {
+        text: "Membership",
+        link: "#"
+      },
+      {
+        text: "Purchase guide",
+        link: "#"
+      },
+      {
+        text: "Privacy policy",
+        link: "#"
+      },
+      {
+        text: "Terms of service",
+        link: "#"
+      }
+    ],
+    social:[
+      {
+        icon:"fa fa-facebook-official",
+        link:"#"
+      },
+      {
+        icon:"fa fa-twitter",
+        link:"#"
+      },
+      {
+        icon:"fa fa-instagram",
+        link:"#"
+      },
+      {
+        icon:"fa fa-linkedin-square",
+        link:"#"
+      }
+    ]
+  }
+});
 Vue.config.devtools = true;
